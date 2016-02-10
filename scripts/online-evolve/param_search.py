1b39c6b12f8c5d56a450114a8836e86d7619ad15
Elte Hupkes <eltehupkes@srxp.com>
2016-02-10 14:56:26 +0100

    Param fixes, more informative output

--- scripts/online-evolve/param_search.py
+++ scripts/online-evolve/param_search.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+
+import random
 import sys
 import os
 import csv
@@ -7,23 +10,23 @@ from start import OnlineEvolutionSupervisor, manager_cmd, analyzer_cmd, \
 # Parameter options to try, from left to right they should lead to
 # more lenient parameters (i.e. bigger chance for populations to survive)
 param_options = {
-    '--initial-population-size': [10, 12, 14, 16],
-    '--initial-age-mu': [3600 / 10.0, 3600 / 7.5, 3600 / 5.0, 3600 / 2.5],
-    '--initial-age-sigma': [3600 / 20.0, 3600 / 15.0, 3600 / 10.0],
+    '--initial-population-size': [8, 10, 12, 14, 16],
+    '--initial-age-mu': [36000.0 / a for a in (15, 10, 7.5, 5, 2.5)],
+    '--initial-age-sigma': [36000.0 / a for a in (30, 20, 15, 10)],
     '--age-cutoff': [0.2, 0.15, 0.1, 0.05],
-    '--mating-fitness-threshold': [0.6, 0.5, 0.4, 0.3],
-    '--gestation-period': [36000 / 20.0, 36000 / 50.0, 36000 / 100.0, 3600 / 150.0],
-    '--max-pair-children': [2, 3, 4, 5, 6],
+    '--mating-fitness-threshold': [0.6, 0.5, 0.4, 0.3, 0.2],
+    '--gestation-period': [36000.0 / a for a in 10, 20, 50, 100, 150, 200],
+    '--max-pair-children': [1, 2, 3, 4, 5, 6],
 }
 
 # Initial picks from the list
 positions = {
-    '--initial-population-size': 1,
+    '--initial-population-size': 2,
     '--initial-age-mu': 1,
     '--initial-age-sigma': 1,
     '--age-cutoff': 2,
     '--mating-fitness-threshold': 2,
-    '--gestation-period': 2,
+    '--gestation-period': 3,
     '--max-pair-children': 2,
 }
 
@@ -68,18 +71,30 @@ for i in range(max_experiments):
                 result -= 1
             elif row[1] == 'extinction':
                 result += 1
+            elif row[1] != 'stable':
+                print("WARNING: Unsupported result `%s`" % row[1],
+                      file=sys.stderr)
 
     # If we have an extinct population we would like to boost it, if not
     # we'll try to decrease an option instead.
-    boost = -1 if result >= 0 else 1
-    attempts = 0
+    if result == 0:
+        print("Stability: try random boost...")
+        boost = -1 if random.random() > 0.5 else 1
+        print("...random boost is %s." % "positive" if boost > 0 else "negative")
+    elif result > 0:
+        print("Extinction: try positive boost.")
+        boost = 1
+    else:
+        print("Explosion: try negative boost.")
+        boost = -1
 
+    attempts = 0
     while True:
         # If there's no key left to boost, just repeat
         # the experiment
         if attempts >= len(keys):
             print("No more adaptations are possible! Rerunning experiment"
-                  " until intervention.")
+                  " until intervention.", file=sys.stderr)
             break
 
         # Boost another parameter
