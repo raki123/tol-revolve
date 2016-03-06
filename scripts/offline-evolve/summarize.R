28149b7579cc2770c12a3ad3d7c954c9c5c13d03
Elte Hupkes <eltehupkes@srxp.com>
2016-03-06 21:49:14 +0100

    Adding baseline to offline evolve plot

--- scripts/offline-evolve/summarize.R
+++ scripts/offline-evolve/summarize.R
@@ -7,11 +7,13 @@ library(ggplot2)
 library(plyr)
 library(reshape)
 
+data0 = read.csv("baseline/generations.csv", head=TRUE)
+data0$param_set = "No evolution"
 data1 = read.csv("plus/generations.csv", head=TRUE)
-data1$param_set = "First"
+data1$param_set = "First param set"
 data2 = read.csv("plus-aggressive/generations.csv", head=TRUE)
-data2$param_set = "Second"
-data = rbind(data1, data2)
+data2$param_set = "Second param set"
+data = rbind(data0, data1, data2)
 data$param_set = as.factor(data$param_set)
 cdata = ddply(data, c("gen", "param_set"), summarise, Fitness=mean(fitness), Velocity=mean(vel), fsd=sd(fitness), vsd=sd(vel))
 #mdata = melt(cdata, id=c("gen", "fsd", "vsd"))
@@ -19,8 +21,8 @@ cdata = ddply(data, c("gen", "param_set"), summarise, Fitness=mean(fitness), Vel
 ggplot(cdata, aes(gen)) +
   geom_line(aes(y=Fitness, colour=param_set)) +
   geom_ribbon(aes(ymin=Fitness-fsd, ymax=Fitness+fsd, fill=param_set), alpha=0.2, linetype=0) +
-  scale_fill_discrete(name="Parameter set") +
-  scale_colour_discrete(name="Parameter set") +
+  scale_fill_discrete(name="Experiment") +
+  scale_colour_discrete(name="Experiment") +
   xlab("Generation") +
   ylab("Fitness")
 
