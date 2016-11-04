19d73db47c2c252de4b2f39f18a852b3518f4da6
Milan Jelisavcic <milan.jelisavcic@gmail.com>
2016-11-04 17:25:09 +0100

    Migrated some of Matteo's code.

---
+++ cpp/tol/plugin/sensor.h
@@ -0,0 +1,33 @@
+#ifndef ACTUATOR_H
+#define ACTUATOR_H
+
+#include "brain/sensor.h"
+#include "revolve/gazebo/sensors/Sensor.h"
+
+namespace tol {
+
+    class Sensor : public revolve::brain::Sensor {
+
+    public:
+        explicit Sensor(revolve::gazebo::SensorPtr sensorPtr)
+                : sensorPtr(sensorPtr) {}
+
+        virtual unsigned int inputs() const {
+            return sensorPtr->inputs();
+        }
+
+        virtual void read(double *input_vector) {
+            sensorPtr->read(input_vector);
+        }
+
+        virtual std::string sensorId() const {
+            return sensorPtr->sensorId();
+        }
+
+    private:
+        revolve::gazebo::SensorPtr sensorPtr;
+    };
+
+}
+
+#endif // ACTUATOR_H
