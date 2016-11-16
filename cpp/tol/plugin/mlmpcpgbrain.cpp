e81918d3d7663ba2653118c61af6e999c86442cc
Matteo De Carlo <matteo.dek@gmail.com>
2016-11-16 12:25:57 +0100

    forgot new mlmp cpg brain class

---
+++ cpp/tol/plugin/mlmpcpgbrain.cpp
@@ -0,0 +1,60 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright (C) 2016  Matteo De Carlo <matteo.dek@covolunablu.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "mlmpcpgbrain.h"
+#include "helper.h"
+
+#include <iostream>
+#include <exception>
+
+using namespace tol;
+
+tol::MlmpCPGBrain::MlmpCPGBrain(std::string robot_name,
+             tol::EvaluatorPtr evaluator,
+             unsigned int n_actuators,
+             unsigned int n_sensors)
+    : revolve::brain::CPGBrain(
+        robot_name,
+        evaluator,
+        n_actuators,
+        n_sensors
+    )
+{
+}
+
+tol::MlmpCPGBrain::~MlmpCPGBrain()
+{
+}
+
+
+
+void MlmpCPGBrain::update(const std::vector< revolve::gazebo::MotorPtr >& actuators,
+                          const std::vector< revolve::gazebo::SensorPtr >& sensors,
+                          double t, double step)
+{
+    try {
+        revolve::brain::CPGBrain::update(
+            Helper::createWrapper(actuators),
+            Helper::createWrapper(sensors),
+            t, step
+        );
+    } catch (std::exception &e) {
+        std::cerr << "exception: " << e.what() << std::endl;
+    }
+}
---
+++ cpp/tol/plugin/mlmpcpgbrain.h
@@ -0,0 +1,48 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright (C) 2016  Matteo De Carlo <matteo.dek@covolunablu.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MLMPCPGBRAIN_H
+#define MLMPCPGBRAIN_H
+
+#include "brain/CPGBrain.h"
+#include "evaluator.h"
+#include "revolve/gazebo/brain/Brain.h"
+
+#include <string>
+#include <vector>
+
+namespace tol {
+
+class MlmpCPGBrain : public revolve::gazebo::Brain, private revolve::brain::CPGBrain
+{
+public:
+    MlmpCPGBrain(std::string robot_name,
+                 EvaluatorPtr evaluator,
+                 unsigned int n_actuators,
+                 unsigned int n_sensors);
+    virtual ~MlmpCPGBrain();
+
+    virtual void update(const std::vector< revolve::gazebo::MotorPtr >& actuators,
+                        const std::vector< revolve::gazebo::SensorPtr >& sensors,
+                        double t, double step) override;
+};
+
+}
+
+#endif // MLMPCPGBRAIN_H
