06cbef48a3598c6b0c4ef3fe745af4c8298c479e
Milan Jelisavcic <milan.jelisavcic@gmail.com>
2017-02-28 18:51:09 +0100

    Moved models to res directory

--- experiments/offline-evolve/start.py
+++ experiments/offline-evolve/start.py
@@ -37,7 +37,7 @@ class OfflineEvolutionSupervisor(Supervisor):
 args = parser.parse_args()
 
 os.environ['GAZEBO_PLUGIN_PATH'] = os.path.join(tol_path, 'build')
-os.environ['GAZEBO_MODEL_PATH'] = os.path.join(tol_path, 'tools', 'models') + \
+os.environ['GAZEBO_MODEL_PATH'] = os.path.join(tol_path, 'res', 'models') + \
                                   ':' + os.path.join(rv_path, 'tools', 'models')
 
 supervisor = OfflineEvolutionSupervisor(
