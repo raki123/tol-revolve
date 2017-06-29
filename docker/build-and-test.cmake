def709cc438f77b9bfc9a64caa3306b488d64bc3
Matteo De Carlo <matteo.dek@gmail.com>
2017-06-29 14:44:52 +0200

    circleci update
    this time it should run just fine

--- docker/build-and-test.cmake
+++ docker/build-and-test.cmake
@@ -1,21 +1,31 @@
 cmake_minimum_required(VERSION 2.8)
 
-set(build_dir ${CMAKE_CURRENT_LIST_DIR}/build)
+set(build_dir ${CMAKE_CURRENT_LIST_DIR}/../build_docker)
 
 if(NOT EXISTS ${build_dir})
   file(MAKE_DIRECTORY ${build_dir})
 endif()
 
 execute_process(
-  COMMAND ${CMAKE_COMMAND} .. -Dtest=ON
+  COMMAND ${CMAKE_COMMAND} ../cpp -Dtest=ON -DREVOLVE_BUILD_PATH="`pwd`/../../revolve/build"
+
   WORKING_DIRECTORY ${build_dir}
+  RESULT_VARIABLE cmake_result
 )
+
+if(${cmake_result})
+  message(FATAL_ERROR "cmake failed")
+endif()
 
 execute_process(
   COMMAND make
   WORKING_DIRECTORY ${build_dir}
+  RESULT_VARIABLE compilation_result
 )
 
+if(${compilation_result})
+  message(FATAL_ERROR "compilation failed")
+endif()
 
 execute_process(
   COMMAND make test
