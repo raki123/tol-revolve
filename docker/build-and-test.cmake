0ca7c37af1e55d03a9f976b5c67774db83a71d01
Matteo De Carlo <matteo.dek@gmail.com>
2017-06-30 13:43:13 +0200

    update revolve_brain code

--- docker/build-and-test.cmake
+++ docker/build-and-test.cmake
@@ -1,13 +1,24 @@
 cmake_minimum_required(VERSION 2.8)
 
 set(build_dir ${CMAKE_CURRENT_LIST_DIR}/../build_docker)
+set(revolve_brain_dir /revolve/revolve-brain)
 
 if(NOT EXISTS ${build_dir})
   file(MAKE_DIRECTORY ${build_dir})
 endif()
 
 execute_process(
-  COMMAND ${CMAKE_COMMAND} ../cpp -Dtest=ON -DREVOLVE_BUILD_PATH=/revolve/revolve/build -DREVOLVE_BRAIN_PATH=/revolve/revolve-brain/cpp/
+  COMMAND git pull
+  WORKING_DIRECTORY ${revolve_brain_dir}
+  RESULT_VARIABLE git_update_result
+)
+
+if(${cmake_result})
+  message(WARNING "git pull failed")
+endif()
+
+execute_process(
+  COMMAND ${CMAKE_COMMAND} ../cpp -Dtest=ON -DREVOLVE_BUILD_PATH=/revolve/revolve/build -DREVOLVE_BRAIN_PATH=${revolve_brain_dir}/cpp
 
   WORKING_DIRECTORY ${build_dir}
   RESULT_VARIABLE cmake_result
